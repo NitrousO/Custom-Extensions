bd294a588eb7e9f6f005d6520845be66fa1ed2c7
NitrousO <150393772+NitrousO@users.noreply.github.com>
2023-11-09 22:12:23 -0500

    Add files via upload

--- extra-music-extension.js
+++ extra-music-extension.js
@@ -15,17 +15,94 @@ class ExtraRealisticMusic {
                 {
                     opcode: 'note',
                     blockType: Scratch.BlockType.REPORTER,
-                    text: 'Note [Notes] With Accidental [Accidentals]',
+                    text: 'Note [Notes]',
                     arguments: {
                         Notes: {
                             type: Scratch.ArgumentType.STRING,
-                            menu: 'NOTES_MENU'
-                        },
-                        Accidentals: {
-                            type: Scratch.ArgumentType.STRING,
-                            menu: 'ACCIDENTALS_MENU'
+                            menu: 'NOTES_MENU',
+                            defaultValue: 'C'
                         }
                     } 
+                },
+                {
+                    opcode: 'accidental',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Accidental [Accidentals]',
+                    arguments: {
+                        Accidentals: {
+                            type: Scratch.ArgumentType.STRING,
+                            menu: 'ACCIDENTALS_MENU',
+                            defaultValue: 'natural'
+                        }
+                    }
+                },
+                {
+                    opcode: 'octave',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Octave [Octave]',
+                    arguments: {
+                        Octave: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '5'
+                        }
+                    }
+                },
+                {
+                    opcode: 'bpm',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'BPM at [BPM]',
+                    arguments: {
+                        BPM: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '120'
+                        }
+                    }
+                },
+                {
+                    opcode: 'measure',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Play on Beat [BEAT] of Measure [MEASURE]',
+                    arguments: {
+                        BEAT: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '1'
+                        },
+                        MEASURE: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '1'
+                        }
+                    }
+                },
+                {
+                    opcode: 'playnote',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Play [NOTE] with Accidental [ACCIDENTAL] and at [OCTAVE] Octave. Play on Beat [beat] of Measure [measure] at [bpm] BPM',
+                    arguments: {
+                        NOTE: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '3'
+                        },
+                        ACCIDENTAL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'natural'
+                        },
+                        OCTAVE: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '5'
+                        },
+                        beat: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '1'
+                        },
+                        measure: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '1'
+                        },
+                        bpm: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: '120'
+                        }
+                    }
                 }
             ],
             menus: {
@@ -78,11 +155,31 @@ class ExtraRealisticMusic {
             } else {
 
                 if (args.Accidentals == 'sharp') {
-                    return args.Notes + 0.5;
+                    return args(Notes + 0.5);
                 }
             }
         }
     }
+    accidental (args) {
+        return args.Accidentals;
+    }
+    octave (args) {
+        return args.Octave;
+    }
+    bpm (args) {
+        return args.BPM;
+    }
+    measure (args) {
+        return args.BEAT;
+        return args.MEASURE;
+    }
+    playnote (args) {
+        if (await Scratch.canFetch('www.youtube.com/watch?v=_q8QmJadSEE')) {
+            const audio = new audio('www.youtube.com/watch?v=_q8QmJadSEE');
+        }
+        vm.setPitch
+    }
+
 }
 Scratch.extensions.register(new ExtraRealisticMusic());
 })(Scratch);
